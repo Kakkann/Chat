fde76e583d6d517eee155cfaf5dd74b7c768c4f2
Kakkann <kaneki.adictter@gmail.com>
2026-09-13 22:23:46 -0300

    feat: add validation to Message attributes

--- domain/Message.cs
+++ domain/Message.cs
@@ -2,12 +2,29 @@ using System.Reflection.Metadata;
 
 class Message(string id, string conversationId, Content content, User sender, DateTime timestamp)
 {
-    public string Id { get; } = id;
-    public string ConversationId { get; } = conversationId;
+    public string Id { get; } = validateString(id);
+    public string ConversationId { get; } = validateString(conversationId);
     public Content Content { get; } = content;
     public User Sender { get; } = sender;
-    public DateTime Timestamp { get; } = timestamp;
+    public DateTime Timestamp { get; } = validateTimestamp(timestamp);
 
-    public Message(string id, string conversationId, Content content, User sender) : this(id, conversationId, content, sender, DateTime.Now){}
+    public Message(string id, string conversationId, Content content, User sender) : this(id, conversationId, content, sender, DateTime.UtcNow){}
     
+    private static DateTime validateTimestamp(DateTime timestamp)
+    {
+        if (timestamp > DateTime.UtcNow)
+        {
+            throw new ArgumentException("Timestamp cannot be in the future.");
+        }
+        return timestamp;
+    }
+
+    private static string validateString(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException("String cannot be null or whitespace.");
+        }
+        return value;
+    }
 }
